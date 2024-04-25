3fd9fbfa2e6bef44b3a51998a61e33771aa39772
chux-da-xteevn <chukwu.steevn@gmail.com>
2024-04-25 00:55:59 +0000

    Task 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,7 @@
-# installing a flask
-# version 2.1.0 mandated
+# install a flask package
+# version 2.1.0
 
 $package_name = 'flask'
 package { $package_name:
-	ensure => '2.1.0',
+    ensure   => '2.1.0' ,
 }
